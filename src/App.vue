4e1b1c20d1a06d124eac949bc03d0bbd834787ad
Sensei-CHO <senseicho@protonmail.com>
2023-01-16 22:48:50 +0100

    Added APP

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Nav class="mb-6" />
+    <IndexBody />
+  </div>
+</template>
+
+<script>
+import IndexBody from './components/IndexBody.vue'
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IndexBody,
+    Nav
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '../public/styles.css'
+createApp(App).mount('#app')
